72630a9e94833384eb48dcd2e345848c28a14c35
Diyorbek Olimov <diyorbekolimov2000@gmail.com>
2023-03-15 14:49:08 +0500

    added sorting

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -5,27 +5,67 @@ import {
   View,
   TextInput,
 } from "react-native";
+import { useState } from "react";
 import { data } from "../data";
 import { styles } from "../styles/HomeStyle";
 
 const Home = ({ navigation }) => {
+  const [search, setSearch] = useState("");
+  const [termsList, setTermsList] = useState(data);
   const { searchInput, termButton, termText } = styles;
+
+  const searchInputHandler = (search: string) => {
+    setSearch(search);
+
+    if (search === "") {
+      const sortedTerms = data.sort((a, b) => {
+        return a.term.localeCompare(b.term);
+      });
+
+      setTermsList(sortedTerms);
+    } else {
+      const newTermsList = data.filter((item) =>
+        item.term.includes(search.toLowerCase())
+      );
+
+      const sortedTerms = newTermsList.sort((a, b) => {
+        const startsWithQuery = a.term
+          .toLowerCase()
+          .startsWith(search.toLowerCase())
+          ? -1
+          : 1;
+        const nameComparison = a.term.localeCompare(b.term);
+        return startsWithQuery || nameComparison;
+      });
+
+      setTermsList(sortedTerms);
+    }
+  };
 
   return (
     <ScrollView>
-      <TextInput placeholder="Qidirish" style={searchInput} />
+      <TextInput
+        placeholder="Qidirish"
+        style={searchInput}
+        value={search}
+        onChangeText={(search) => searchInputHandler(search)}
+      />
       <View>
-        {data.map((item, index) => (
-          <TouchableOpacity
-            key={index}
-            style={termButton}
-            onPress={() => {
-              navigation.navigate("Termin", { termID: item.id });
-            }}
-          >
-            <Text style={termText}>{item.term}</Text>
-          </TouchableOpacity>
-        ))}
+        {termsList.length ? (
+          termsList.map((item, index) => (
+            <TouchableOpacity
+              key={index}
+              style={termButton}
+              onPress={() => {
+                navigation.navigate("Termin", { termID: item.id });
+              }}
+            >
+              <Text style={termText}>{item.term}</Text>
+            </TouchableOpacity>
+          ))
+        ) : (
+          <Text>Termin topilmadi!</Text>
+        )}
       </View>
     </ScrollView>
   );
