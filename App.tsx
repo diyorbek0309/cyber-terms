c31b2d500a2897e5b3081bcdf76ec1e477378a52
Diyorbek Olimov <diyorbekolimov2000@gmail.com>
2023-03-16 16:18:15 +0500

    added-dark-mode-1

--- App.tsx
+++ App.tsx
@@ -1,17 +1,42 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useState } from "react";
+import { TouchableOpacity, Image } from "react-native";
 import CTermItem from "./src/components/CTermItem";
 import HomeScreen from "./src/screens/Home";
+import { eColors, eImages } from "./src/types/enum";
+import { ThemeContext } from "./src/services/ThemeContext";
 
-const { Navigator, Screen } = createNativeStackNavigator();
+const { Navigator, Screen, Group } = createNativeStackNavigator();
 
 function App() {
+  const [isLight, setIsLight] = useState(true);
+
   return (
     <NavigationContainer>
-      <Navigator>
-        <Screen name="Bosh sahifa" component={HomeScreen} />
-        <Screen name="Termin" component={CTermItem} />
-      </Navigator>
+      <ThemeContext.Provider value={{ isLight }}>
+        <Navigator>
+          <Group
+            screenOptions={({ navigation }) => ({
+              headerStyle: {
+                backgroundColor: isLight ? eColors.WHITE : eColors.BLACK,
+              },
+              headerTitleStyle: {
+                color: isLight ? eColors.BLACK : eColors.WHITE,
+              },
+              headerRight: () => (
+                <TouchableOpacity onPress={() => setIsLight(!isLight)}>
+                  <Image source={isLight ? eImages.MOON : eImages.SUN} />
+                </TouchableOpacity>
+              ),
+              headerTintColor: isLight ? eColors.BLACK : eColors.WHITE,
+            })}
+          >
+            <Screen name="Bosh sahifa" component={HomeScreen} />
+            <Screen name="Termin" component={CTermItem} />
+          </Group>
+        </Navigator>
+      </ThemeContext.Provider>
     </NavigationContainer>
   );
 }
---
+++ src/types/enum.ts
@@ -0,0 +1,65 @@
+enum eScreens {
+  HOME = "Svoyak Calculator",
+  SVOYAK = "Shaxsiy oʻyin",
+  EKVARTET = "Erudit-kvartet",
+  RESULTS = "Natijalar",
+}
+
+enum eSvoyak {
+  TITLE = "Oʻyin nomi",
+  DEFAULT_NAME = "ishtirokchi",
+}
+
+enum eColors {
+  WHITE = "#76ffff",
+  BLACK = "#001e3c",
+}
+
+enum eImages {
+  MOON = require("../../assets/moon_icon.png"),
+  SUN = require("../../assets/sun_icon.png"),
+}
+
+const icons = ["🥇", "🥈", "🥉", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣", "🔟"];
+
+const scoresList = ["-", "10", "20", "30", "40", "50"];
+
+const defaultData = [
+  {
+    id: 0,
+    name: `1-${eSvoyak.DEFAULT_NAME}`,
+    scores: "",
+    numberOfLines: 2,
+    isActive: false,
+  },
+  {
+    id: 1,
+    name: `2-${eSvoyak.DEFAULT_NAME}`,
+    scores: "",
+    numberOfLines: 2,
+    isActive: false,
+  },
+  {
+    id: 2,
+    name: `3-${eSvoyak.DEFAULT_NAME}`,
+    scores: "",
+    numberOfLines: 2,
+    isActive: false,
+  },
+  {
+    id: 3,
+    name: `4-${eSvoyak.DEFAULT_NAME}`,
+    scores: "",
+    numberOfLines: 2,
+    isActive: false,
+  },
+  {
+    id: 4,
+    name: `5-${eSvoyak.DEFAULT_NAME}`,
+    scores: "",
+    numberOfLines: 2,
+    isActive: false,
+  },
+];
+
+export { eScreens, eSvoyak, icons, scoresList, defaultData, eColors, eImages };
