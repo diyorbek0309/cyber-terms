8527e16c1f74d482d36c5c7524b0c1c6d897f02f
Diyorbek Olimov <diyorbekolimov2000@gmail.com>
2023-03-24 15:56:53 +0500

    some changes

--- src/types/enum.ts
+++ src/types/enum.ts
@@ -20,46 +20,4 @@ enum eImages {
   SUN = require("../../assets/sun_icon.png"),
 }
 
-const icons = ["🥇", "🥈", "🥉", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣", "🔟"];
-
-const scoresList = ["-", "10", "20", "30", "40", "50"];
-
-const defaultData = [
-  {
-    id: 0,
-    name: `1-${eSvoyak.DEFAULT_NAME}`,
-    scores: "",
-    numberOfLines: 2,
-    isActive: false,
-  },
-  {
-    id: 1,
-    name: `2-${eSvoyak.DEFAULT_NAME}`,
-    scores: "",
-    numberOfLines: 2,
-    isActive: false,
-  },
-  {
-    id: 2,
-    name: `3-${eSvoyak.DEFAULT_NAME}`,
-    scores: "",
-    numberOfLines: 2,
-    isActive: false,
-  },
-  {
-    id: 3,
-    name: `4-${eSvoyak.DEFAULT_NAME}`,
-    scores: "",
-    numberOfLines: 2,
-    isActive: false,
-  },
-  {
-    id: 4,
-    name: `5-${eSvoyak.DEFAULT_NAME}`,
-    scores: "",
-    numberOfLines: 2,
-    isActive: false,
-  },
-];
-
-export { eScreens, eSvoyak, icons, scoresList, defaultData, eColors, eImages };
+export { eScreens, eSvoyak, eColors, eImages };
