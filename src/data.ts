0a1eb10ae452e33b7eb0022ce7049dce0b57369a
Diyorbek Olimov <diyorbekolimov2000@gmail.com>
2023-03-27 15:54:18 +0500

    added some data

--- src/data.ts
+++ src/data.ts
@@ -1,21 +1,163 @@
 export const uz = [
-  { id: 0, term: "autentifikatsiya", description: "Nimadirla" },
-  { id: 1, term: "identifikatsiya", description: "Nimadirla" },
+  {
+    id: 0,
+    term: "autentifikatsiya",
+    description: "Foydalanuvchi identifikatsiyasini tasdiqlash",
+  },
+  {
+    id: 1,
+    term: "identifikatsiya",
+    description: "Foydalanuvchi aniqlash jarayoni",
+  },
   {
     id: 2,
     term: "avtorizatsiya",
     description:
-      "Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla",
+      "Foydalanuvchi tomonidan amalga oshirilgan har qanday operatsiyalar uchun ruxsat berish",
+  },
+  { id: 3, term: "domen", description: "Tarmoqqa ulanish uchun nom" },
+  { id: 4, term: "shifrlash", description: "Ma'lumotlarni himoyalash usuli" },
+  {
+    id: 5,
+    term: "de-shifrlash",
+    description:
+      "Shifrlangan ma'lumotlarni boshqa usullar orqali oqib oluvchi jarayon",
+  },
+  {
+    id: 6,
+    term: "firewall",
+    description: "Tarmoqni himoyalashni ta'minlaydigan dasturiy vosita",
+  },
+  {
+    id: 7,
+    term: "virus",
+    description:
+      "O'zaro almashinuvchan ma'lumotlar, fayllar yoki dasturlar, boshqa dasturlarning ishlashiga ta'sir qilish",
+  },
+  { id: 8, term: "malware", description: "Zararli dasturlar va fayllar" },
+  {
+    id: 9,
+    term: "phishing",
+    description:
+      "Maxfiy ma'lumotlarni olish maqsadida elektron pochtalar, xabarlar yoki havolalar yordamida foydalanuvchilarni qo'llab-quvvatlash",
+  },
+  {
+    id: 10,
+    term: "hacking",
+    description: "Tizimlarga yoki kompyuterlarga maxfiy ravishda kirish",
+  },
+  {
+    id: 11,
+    term: "ransomware",
+    description:
+      "Fayllarni shifrlash yoki o'chirib tashlash va foydalanuvchidan pul so'ralish maqsadida qo'llaniladigan virus",
+  },
+  {
+    id: 12,
+    term: "vayrless-harakat",
+    description:
+      "Ko'plab kabellarga xaydalanuvchining ko'pincha iste'mol qilinadigan IoT qurilmalaridan kelib chiqqan foydalanuvchilar uchun maxfiylik xavfsizlik muammo.",
+  },
+  {
+    id: 13,
+    term: "penetratsiya sinovlari",
+    description:
+      "Tarmoq xavfsizligini tekshirish uchun tuzilgan sinovlar. Bu sinovlarning asosiy maqsadi tarmoqdagi xavfsizlik yomonliklarini aniqlash va bu yomonliklarni tuzatish uchun imkoniyat yaratishdir.",
+  },
+  {
+    id: 14,
+    term: "VPN",
+    description:
+      "Virtual maxfiy tarmoq, tarmoqga internet orqali ulanish uchun yordam beradi, lekin foydalanuvchining ma'lumotlarini maxfiy ko'rib turadi va uni himoya qiladi.",
+  },
+  {
+    id: 15,
+    term: "DNS tuslari",
+    description:
+      "Ko'plab DNS-serverlarni muvofiqlashtiradigan tarzda tuzilgan to'g'ridan-to'g'ri so'rovlarni yo'naltirish usuli.",
+  },
+  {
+    id: 16,
+    term: "DDoS hamyonchiligi",
+    description:
+      "Tarmoq xizmatlari orqali ko'plab to'g'ridan-to'g'ri so'rovlar yuborilishi bilan tarmoq xizmatlari buzilishi",
+  },
+  {
+    id: 17,
+    term: "malware",
+    description:
+      "Maxsus maqsadlar uchun yaratilgan dasturiy ta'minotlarning bitta turi. Bu dasturiy ta'minotlar foydalanuvchining ruxsatidan tashqari tarqalgan holda ishga tushiriladi va kompyuterni xavfsizlik rivojini buzib qo'yadi.",
   },
 ];
 
 export const en = [
-  { id: 0, term: "autentification", description: "Nimadirla" },
-  { id: 1, term: "identification", description: "Nimadirla" },
+  {
+    id: 0,
+    term: "DDoS attack",
+    description:
+      "A form of cyberattack that involves flooding a network or website with high volumes of traffic to disrupt normal operation.",
+  },
+  {
+    id: 1,
+    term: "malware",
+    description:
+      "A type of malicious software designed to infiltrate and harm computer systems without the user's consent, compromising the security of the system.",
+  },
   {
     id: 2,
-    term: "authorization",
+    term: "wireless insecurity",
     description:
-      "Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla Nimadirla",
+      "A security issue for users of IoT devices that often use multiple channels, leading to security vulnerabilities and data breaches.",
+  },
+  {
+    id: 3,
+    term: "penetration testing",
+    description:
+      "A form of security testing used to evaluate the security of a computer system or network by simulating an attack from a malicious source.",
+  },
+  {
+    id: 4,
+    term: "VPN",
+    description:
+      "A virtual private network that provides secure internet access by routing internet connection through a server that encrypts and protects user data.",
+  },
+  { id: 5, term: "DNS spoofing", description: "A method of" },
+  {
+    id: 6,
+    term: "authentication",
+    description: "Verification of user identification",
+  },
+  {
+    id: 7,
+    term: "identification",
+    description: "Process of identifying a user",
+  },
+  {
+    id: 8,
+    term: "authorization",
+    description: "Granting permission by a user to perform any operation",
+  },
+  { id: 9, term: "domain", description: "Name for network connection" },
+  {
+    id: 10,
+    term: "encryption",
+    description: "Method of protecting information",
+  },
+  {
+    id: 11,
+    term: "decryption",
+    description:
+      "Process of decoding encrypted information using other methods",
+  },
+  {
+    id: 12,
+    term: "firewall",
+    description: "Software used to protect networks",
+  },
+  {
+    id: 13,
+    term: "virus",
+    description:
+      "Malicious programs, files, or codes that can impact the functioning of",
   },
 ];
